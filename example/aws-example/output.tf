af1a857002536a33982eef69165708ca88d83c9e
vagrant <c1t1d0s7@gmail.com>
2022-08-26 23:45:42 +0000

    add

--- example/aws-example/output.tf
+++ example/aws-example/output.tf
@@ -2,7 +2,7 @@ output "public_ip" {
   value = aws_instance.my_instance.public_ip
 }
 
-#output "elastic_ip" {
-#  value = aws_eip.my_eip.public_ip
-#}
+output "elastic_ip" {
+  value = aws_eip.my_eip.public_ip
+}
 
